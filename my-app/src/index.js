8c795994c0d340b87c7c9fbc935be245efcd6bff
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-02-27 22:30:53 -0500

    C2: Now Squareworks with props instead of state. Erased constructor also

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
+class Square extends React.Component {  
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
